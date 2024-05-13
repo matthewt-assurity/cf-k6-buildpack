7e8c6ce03948439bd7fd161a79e373f400fa7e51
matthewt-assurity <matthew.tansley@assurity.co.nz>
2024-05-13 15:46:48 +1200

    Added initial files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cf-k6-buildpack
+
 Buildpack to install k6 to a Cloud Foundry app
+
+# License
+
+MIT License
---
+++ parameters.sh
@@ -0,0 +1,7 @@
+# This file is for config variables, and is designed to be sourced by the buildpack scripts.
+
+# k6 setup
+K6_FILE=k6
+
+# Dependencies path
+K6_FILE="${DEPS_DIR}/${DEPS_IDX}/k6"

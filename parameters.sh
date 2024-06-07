9840b948c447f423376fe5065cb1e1c0c7924948
matthewt-assurity <matthew.tansley@assurity.co.nz>
2024-06-07 14:56:16 +1200

    Updated k6 file path name

--- parameters.sh
+++ parameters.sh
@@ -1,7 +1,7 @@
 # This file is for config variables, and is designed to be sourced by the buildpack scripts.
 
 # k6 setup
-K6_FILE=k6
+K6_FILE=k6.exe
 
 # Dependencies path
-K6_DIR="${DEPS_DIR}/${DEPS_IDX}/k6"
+K6_DIR="${DEPS_DIR}/${DEPS_IDX}/k6.exe"
